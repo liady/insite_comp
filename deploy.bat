e6cf67e771ae14437a886fdb68d0802b212684fb
Liad Yosef <liad.yosef@gmail.com>
2015-02-09 22:20:03 +0200

    commit

---
+++ deploy.bat
@@ -0,0 +1,1 @@
+call aws s3 sync dist/. s3://dm-util/insite-contest --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
---
+++ push.bat
@@ -0,0 +1,3 @@
+git add -A
+git commit -a -m "commit"
+git push
