9c9e4e743f114b85ab92f8658061ec589b9e7ac1
Liad Yosef <liad.yosef@gmail.com>
2015-02-09 22:12:26 +0200

    initial

---
+++ build.bat
@@ -0,0 +1,2 @@
+call uglifyjs app\insite.april.js -m -o dist\insite.april.min.js --source-map dist\insite.april.min.js.map
+call uglifyjs app\insite.monday.js -m -o dist\insite.monday.min.js --source-map dist\insite.monday.min.js.map
